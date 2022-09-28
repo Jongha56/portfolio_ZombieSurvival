5ec2b180241e4fbba2054856ee9e2142c4cb797e
Jongha Jeong <wjdwhdgk1@gmail.com>
2022-09-28 16:10:24 +0900

    Create Manager Folder of Zombie Survival Game

---
+++ Manager/UIManager.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; // 씬 관리자 관련 코드
+using UnityEngine.UI; // UI 관련 코드
+
+//UIManager는 다른 스크립트가 HUD Canvas 게임 오브젝트의 각 UI요소를 편리하게 접근해서 쉽게 갱신하도록 접근 경로를 만드는 스크립트이다.
+// 필요한 UI에 즉시 접근하고 변경할 수 있도록 허용하는 UI 매니저
+public class UIManager : MonoBehaviour {
+    // 싱글톤 접근용 프로퍼티
+    public static UIManager instance
+    {
+        get
+        {
+            if (m_instance == null)
+            {
+                //최초 한번만 UIManager 타입의 오브젝트 할당
+                m_instance = FindObjectOfType<UIManager>();
+            }
+            //이후에는 할당 없이 m_instance값 반환
+            return m_instance;
+        }
+    }
+
+    private static UIManager m_instance; // 싱글톤이 할당될 변수
+
+    public Text ammoText; // 탄약 표시용 텍스트
+    public Text scoreText; // 점수 표시용 텍스트
+    public Text waveText; // 적 웨이브 표시용 텍스트
+    public GameObject gameoverUI; // 게임 오버시 활성화할 UI 
+
+    // 탄약 텍스트 갱신
+    public void UpdateAmmoText(int magAmmo, int remainAmmo) {
+        ammoText.text = magAmmo + "/" + remainAmmo;
+    }
+
+    // 점수 텍스트 갱신
+    public void UpdateScoreText(int newScore) {
+        scoreText.text = "Score : " + newScore;
+    }
+
+    // 적 웨이브 텍스트 갱신
+    public void UpdateWaveText(int waves, int count) {
+        waveText.text = "Wave : " + waves + "\nEnemy Left : " + count;
+    }
+
+    // 게임 오버 UI 활성화
+    public void SetActiveGameoverUI(bool active) {
+        gameoverUI.SetActive(active);
+    }
+
+    // 게임 재시작
+    public void GameRestart() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
